50ad4cc3be3d6d6017f7335b020370c84a35a7d8
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-02-17 21:53:14 +0100

    Import GlobalStyles

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import GlobalStyles from './GlobalStyles.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyles />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
